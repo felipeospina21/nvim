9e1d209d9085f9571fd112db38b1729bfd2d34d9
Felipe Ospina <felipeospina21@hotmail.com>
2023-04-07 18:17:20 -0500

    update theme, add tsx extensions

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,11 +9,22 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("user.plugins.treesitter")
-    end,
-  },
   "windwp/nvim-autopairs",
+  { "catppuccin/nvim",  name = "catppuccin", enabled = false },
+  { "rose-pine/neovim", name = "rose-pine",  opts = { variant = "moon" }, enabled = false },
+  {
+    'uloco/bluloco.nvim',
+    lazy = false,
+    priority = 1000,
+    dependencies = { 'rktjmp/lush.nvim' },
+    opts = { style = "dark" },
+    enabled = false
+  },
+  { "folke/tokyonight.nvim", name = "tokyonight", opts = { style = "storm" } },
+  {
+    "maxmellon/vim-jsx-pretty",
+    name = "vim-jsx-pretty",
+    dependencies = { "HerringtonDarkholme/yats.vim", "yuezk/vim-js" },
+    event = { "BufEnter *.tsx", "BufEnter *.ts" },
+  }
 }
