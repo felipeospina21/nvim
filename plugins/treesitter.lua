31afb4f091b1f4608076d21e16323774d76c7652
Felipe Ospina <felipe.ospina.-nd@disney.com>
2023-04-11 18:43:08 -0500

    add default lsp & new plugins

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,14 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
-    autotag = {
-      enable = true,
-      filetypes = {
-        'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx',
-        'rescript',
-        'css', 'lua', 'xml', 'php', 'markdown'
-      },
-    },
+    ensure_installed = { "typescript", "html" },
+    autotag = { enable = true }
+    -- autotag = {
+    --   enable = true,
+    --   filetypes = {
+    --     'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx',
+    --     'rescript',
+    --     'css', 'lua', 'xml', 'php', 'markdown'
+    --   },
+    -- },
   }
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,17 +1,5 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  "windwp/nvim-autopairs",
-  { "catppuccin/nvim",  name = "catppuccin", enabled = false },
-  { "rose-pine/neovim", name = "rose-pine",  opts = { variant = "moon" }, enabled = false },
+  { "rose-pine/neovim",      name = "rose-pine",  opts = { variant = "moon" }, enabled = false },
   {
     'uloco/bluloco.nvim',
     lazy = false,
@@ -26,5 +14,11 @@ return {
     name = "vim-jsx-pretty",
     dependencies = { "HerringtonDarkholme/yats.vim", "yuezk/vim-js" },
     event = { "BufEnter *.tsx", "BufEnter *.ts" },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = { filetypes = { "tsx", "typescriptreact" } }
   },
 }
