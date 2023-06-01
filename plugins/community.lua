ae96e2f7f7320f5600c4ce545766ce71246610ab
Felipe Ospina <felipe.ospina.-nd@disney.com>
2023-06-01 13:21:03 -0500

    add go community pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,6 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.editing-support.treej" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.pack.go" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
