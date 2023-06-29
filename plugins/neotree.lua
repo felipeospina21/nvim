33c27f09bc74b1580e0547ed2b73e5262c9b5365
Felipe Ospina <felipe.ospina.-nd@disney.com>
2023-06-29 14:45:37 -0500

    update neo-tree window size

--- plugins/neotree.lua
+++ plugins/neotree.lua
@@ -26,5 +26,18 @@ return {
         handler = on_file_remove,
       },
     }
+    opts.window = {
+      width = 50,
+      mappings = {
+        ["<space>"] = false, -- disable space until we figure out which-key disabling
+        ["[b"] = "prev_source",
+        ["]b"] = "next_source",
+        o = "open",
+        O = "system_open",
+        h = "parent_or_close",
+        l = "child_or_open",
+        Y = "copy_selector",
+      },
+    }
   end,
 }
