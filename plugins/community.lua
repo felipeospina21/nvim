c08fcb1cf926c728fb5529809a921e0781088397
felipe ospina <felipeospina21@hotmail.com>
2023-09-01 18:22:16 -0500

    add bash community pack plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,10 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.editing-support.treej" },
+  { import = "astrocommunity.editing-support.treesj" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.colorscheme.rose-pine" },
   {
     "rose-pine",
@@ -17,9 +18,4 @@ return {
       },
     },
   },
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
