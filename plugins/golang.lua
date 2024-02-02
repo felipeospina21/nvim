a66f7d0d1d081c9a7b19ba3dccdfc714e3c9467b
felipe ospina <felipeospina21@hotmail.com>
2024-02-02 16:14:31 -0500

    update golang plugin

--- plugins/golang.lua
+++ plugins/golang.lua
@@ -33,15 +33,25 @@ return {
     opts = {},
   },
   {
-    "ray-x/go.nvim",
+    "olexsmir/gopher.nvim",
     dependencies = {
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
+      "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    opts = {},
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()',
+    ft = { "go" },
   },
+  -- {
+  --   "ray-x/go.nvim",
+  --   dependencies = {
+  --     "ray-x/guihua.lua",
+  --     "neovim/nvim-lspconfig",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   opts = {
+  --     -- diagnostics = false,
+  --   },
+  --   event = { "CmdlineEnter" },
+  --   ft = { "go", "gomod" },
+  --   build = ':lua require("go.install").update_all_sync()',
+  -- },
 }
